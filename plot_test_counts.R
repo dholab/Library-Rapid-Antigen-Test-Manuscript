8a358125fc0ad0a4d94f4c67a9fa44440a1ce389
Nicholas Minor <nrminor@wisc.edu>
2024-05-08 10:26:45 -0500

    adding a couple early bar plots

--- plot_test_counts.R
+++ plot_test_counts.R
@@ -3,20 +3,42 @@
 library(tidyverse)
 library(ggplot2)
 
-csv_path <- "RATs_grouped_by_month.csv"
+# 
+csv_path <- "data/RATs_grouped_by_month.csv"
 
-test_counts <- read_csv(csv_path, trim_ws = TRUE, show_col_types = FALSE)
+# 
+test_counts <- read_csv(csv_path, trim_ws = TRUE, show_col_types = FALSE) |>
+  select(-`...5`)
 
-cleaned_counts <- test_counts |>
-  select(-c(`...5`, `# envelopes sent out`)) |>
+#
+all_counts_plot <- test_counts |>
   pivot_longer(
     cols = -Month,
     names_to = "Status",
     values_to = "Count"
-  )
-
-cleaned_counts |>
+  ) |>
   ggplot(aes(x = Month, y = Count, fill = Status)) +
   geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
   labs(title = "Count of Tests by Status per Month", x = "Month", y = "Count") +
   theme_minimal()
+
+#
+ggsave("visuals/all_counts_plot.pdf", all_counts_plot,
+       height = 6, width = 8)
+
+# 
+two_counts_plot  <- test_counts |>
+  select(-`# envelopes sent out`) |>
+  pivot_longer(
+    cols = -Month,
+    names_to = "Status",
+    values_to = "Count"
+  ) |>
+  ggplot(aes(x = Month, y = Count, fill = Status)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
+  labs(title = "Count of Tests by Status per Month", x = "Month", y = "Count") +
+  theme_minimal()
+
+#
+ggsave("visuals/two_counts_plot.pdf", all_counts_plot,
+       height = 6, width = 8)
