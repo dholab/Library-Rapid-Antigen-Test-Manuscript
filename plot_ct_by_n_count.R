5f95c6307c9e18dad4853517945c99426d898b4a
Nicholas Minor <nrminor@wisc.edu>
2024-05-20 15:05:40 -0500

    switching to making a scatterplot with coverage greater than 10x, plus more data

--- plot_ct_by_n_count.R
+++ plot_ct_by_n_count.R
@@ -4,41 +4,26 @@ library(tidyverse)
 library(ggplot2)
 
 # user inputs
-n_counts_path <- "data/final_counts.csv"
-cts_path <- "data/RAT_Ct_Data.txt"
+cts_path <- "data/ct_by_cov_percentage.tsv"
 
 # read in the dataframes while trimming any unintentional whitespace
-n_counts_df <- read_csv(n_counts_path, trim_ws = TRUE, show_col_types = FALSE)
 ct_df <- read_tsv(cts_path, trim_ws = TRUE, show_col_types = FALSE)
-
-# clean the n counts
-percentages <- n_counts_df |>
-  rename(Sample_ID = `#Filename`) |>
-  mutate(Sample_ID = str_remove(Sample_ID, ".fa")) |>
-  select(Sample_ID, Total, N) |>
-  mutate(N_percentage = (N / Total) * 100)
 
 # clean the ct dataframe
 clean_cts <- ct_df |>
-  mutate(Ct = as.numeric(Ct))
-
-# join the two together
-plotting_df <- percentages |>
-  left_join(clean_cts, by = join_by(Sample_ID)) |>
-  filter(!is.na(Ct)) |>
-  rename(Passing = `Pass?`) |>
-  rename(N_count = N)
+  mutate(`Ct Value` = as.numeric(`Ct Value`)) |>
+  filter(!is.na(`Ct Value`))
 
 # write out the plotting data for transparency
-write_tsv(plotting_df, "data/ct_by_n_plotting_data.tsv")
+# write_tsv(clean_cts, "data/ct_by_n_plotting_data.tsv")
 
 # plot them
-ct_by_n <- plotting_df |>
-  ggplot(aes(x = Ct, y = N_percentage)) +
+ct_by_n <- clean_cts |>
+  ggplot(aes(x = `Ct Value`, y = `% Coverage at >10x Depth`)) +
   geom_point(size = 2.5, aes(color = Passing)) +
   # geom_smooth(method = "loess", se = TRUE, show.legend = FALSE) +
-  labs(x = "qPCR Cycle Threshold Value", y = "Percent ambiguous bases") +
+  labs(x = "qPCR Cycle Threshold Value") +
   theme_linedraw()
 
 # export the plot
-ggsave("visuals/ct_by_n.pdf", ct_by_n, height = 5, width = 7)
+ggsave("visuals/ct_by_cov.pdf", ct_by_n, height = 5, width = 7)
